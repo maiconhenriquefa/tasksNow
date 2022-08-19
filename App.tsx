84797176c6e3aa008c495b5b51cff84f095e64c1
Maicon Henrique <maiconhenriquefa@gmail.com>
2022-08-18 23:48:20 -0300

    added logo svg

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Login from './src/screens/login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Project base</Text>
       <StatusBar style="auto" />
+      <Login />
     </View>
   );
 }
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LogoTasksNowIcon from '../../assets/logo.svg';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <LogoTasksNowIcon />
+    </View>
+  );
+}
