3e073b3adbf528625622962baac9a042be64eeb0
Maicon Henrique <maiconhenriquefa@gmail.com>
2022-09-07 21:57:28 -0300

    theme global

---
+++ src/theme/colors.ts
@@ -0,0 +1,29 @@
+export default {
+  colors: {
+    primary100: '#E7A600',
+    primary200: '#CB9900',
+    primary300: '#E7A600',
+    primary400: '#CB9900',
+
+    secondary100: '#6B7B89',
+    secondary200: '#2B4257',
+    secondary300: '#25384A',
+    secondary400: '#1E2E3D',
+    secondary500: '#111A23',
+    
+    neutral50: "#FDFDFD",
+    neutral100: '#E5E5E5',
+    neutral200: '#C2C2C2',
+    neutral300: '#747474',
+    neutral400: '#383838',
+
+    danger100: '#EDA6A6',
+    danger200: '#D33131',
+    danger300: '#B10E0E',
+
+    contrast0: '#FFFFFF',
+    contrast100: '#000000',
+
+    transparent: '#FFFFFF00'
+  },
+}
---
+++ src/theme/shadow.ts
@@ -0,0 +1,11 @@
+export default {
+  shadow: {
+    primaryShadow: {
+      'shadow-color': '#000',
+      'shadow-offset': {width: 0, height: '2px'},
+      'shadow-opacity': '0.25px',
+      'shadow-radius': '4px',
+      'elevation': '4px'
+    },
+  },
+}
